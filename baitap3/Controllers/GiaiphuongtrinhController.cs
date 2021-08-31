01bfcd393d37befe2f784fbff0a6bc3d6d15dc47
My PC <My PC@DESKTOP-IBQBT32>
2021-08-31 13:30:10 +0700

    1

---
+++ baitap3/Controllers/GiaiphuongtrinhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap3.Controllers
+{
+    public class GiaiphuongtrinhController : Controller
+    {
+        // GET: Giaiphuongtrinh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ baitap3/Views/Giaiphuongtrinh.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap3.Views
+{
+    public class Giaiphuongtrinh
+    {
+        public int SoA { get; set; }
+        public int SoB { get; set; }
+        public int TinhTong(int SoA,int SoB)
+    }
+}
