d0fcd272bc7ad6e88191cbcd64b56596af849a73
My PC <My PC@DESKTOP-IBQBT32>
2021-09-09 23:16:00 +0700

    4

---
+++ baitap3/Models/LapTrinhQuanLyDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace baitap3.Models
+{
+    public class LapTrinhQuanLyDbcontext : DbContext
+    {
+
+        public LapTrinhQuanLyDbcontext() : base("name=LTQLDbContext")
+        { }
+        public virtual DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Student>()
+            .Property(e => e.StudentName)
+            .IsUnicode(false);
+
+        }
+    }
+}
---
+++ baitap3/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace baitap3.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+       [Key]
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+}
