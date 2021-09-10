4e34baf0aed52cc8aa20fe2eaa27cbd81f25706a
My PC <My PC@DESKTOP-IBQBT32>
2021-09-10 14:08:39 +0700

    6

---
+++ baitap3/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using baitap3.Models;
+
+namespace baitap3.Controllers
+{
+    public class StudentController : Controller
+    {
+        LapTrinhQuanLyDbcontext db = new LapTrinhQuanLyDbcontext();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var model = db.Students.ToList()''
+            return View(Modol)
+        }
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
